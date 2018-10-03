ea3809d1021dc6c5b15cd30bb2dded83ecca8d05
wfhsiao <wfhsiao@users.noreply.github.com>
2018-10-03 11:49:23 +0800

    Add files via upload

---
+++ session_variables/Default3.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Default3 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        DateTime dt = DateTime.Today;
+        DateTime dt2 = DateTime.Now;
+        Response.Output.Write("{0}<br />", dt.ToShortDateString());
+        dt = dt.AddDays(7);
+        Response.Output.Write("{0:d}<br />", dt);
+        Response.Write(dt2);
+    }
+}
