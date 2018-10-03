3ff14715e18b6a7ea193a3913224dfb1ec18f7a2
wfhsiao <wfhsiao@users.noreply.github.com>
2018-10-03 11:04:11 +0800

    Add files via upload

--- session_variables/WebSite1/Default2.aspx.cs
+++ session_variables/WebSite1/Default2.aspx.cs
@@ -9,7 +9,17 @@ public partial class Default2 : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        if (! IsPostBack)  nextQ();
+        if (!IsPostBack)
+        {
+            myInit();
+            nextQ();
+        }
+    }
+
+    private void myInit()
+    {
+        Session["correct"] = 0;
+        Session["current"] = 0;
     }
 
     private void nextQ()
@@ -28,9 +38,25 @@ public partial class Default2 : System.Web.UI.Page
         int ans = int.Parse(TextBox1.Text);
         int number1 = int.Parse(Session["number1"].ToString());
         int number2 = int.Parse(Session["number2"].ToString());
-        if ( number1 * number2 == ans)
+        int correct = int.Parse(Session["correct"].ToString());
+        if (number1 * number2 == ans)
+        {
             msg.Text += string.Format("{0} * {1} = {2} 答對了！<br />", number1, number2, ans);
+            correct += 1;
+            Session["correct"] = correct ;
+        }
         else
-            msg.Text += string.Format("{0} * {1} = {2} 答錯了！ 正确答案是{3} <br />", number1, number2, ans, number1* number2);
+            msg.Text += string.Format("{0} * {1} = {2} 答錯了！ 正确答案是{3} <br />", number1, number2, ans, number1 * number2);
+
+        int current = int.Parse(Session["current"].ToString()) + 1;
+        Session["current"] = current ;
+        if (current==3)
+        {
+            msg.Text += string.Format("得分：{0}, <a href='{1}'>重玩</a>", 100 * correct / current, System.IO.Path.GetFileName(System.Web.HttpContext.Current.Request.Url.AbsolutePath));
+        } else
+        {
+            TextBox1.Text = "";
+            nextQ();
+        }
     }
 }
