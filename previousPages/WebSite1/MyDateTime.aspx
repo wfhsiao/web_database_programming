b47f3fb36c0c7c7c621c76020e4bada2a09fe574
wfhsiao <wfhsiao@users.noreply.github.com>
2018-10-17 11:38:52 +0800

    Add files via upload

---
+++ previousPages/WebSite1/MyDateTime.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="MyDateTime.aspx.cs" Inherits="MyDateTime" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Calendar ID="Calendar1" runat="server" BackColor="#FFFFCC" BorderColor="#FFCC66" BorderWidth="1px" DayNameFormat="Shortest" Font-Names="Verdana" Font-Size="8pt" ForeColor="#663399" Height="200px" OnSelectionChanged="Calendar1_SelectionChanged" ShowGridLines="True" Width="220px">
+            <DayHeaderStyle BackColor="#FFCC66" Font-Bold="True" Height="1px" />
+            <NextPrevStyle Font-Size="9pt" ForeColor="#FFFFCC" />
+            <OtherMonthDayStyle ForeColor="#CC9966" />
+            <SelectedDayStyle BackColor="#CCCCFF" Font-Bold="True" />
+            <SelectorStyle BackColor="#FFCC66" />
+            <TitleStyle BackColor="#990000" Font-Bold="True" Font-Size="9pt" ForeColor="#FFFFCC" />
+            <TodayDayStyle BackColor="#FFCC66" ForeColor="White" />
+        </asp:Calendar>
+        <br />
+        <br />
+        <asp:Literal ID="msg" runat="server"></asp:Literal>
+        <br />
+        <br />
+        <asp:RadioButtonList ID="RadioButtonList1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="RadioButtonList1_SelectedIndexChanged" RepeatColumns="5" RepeatDirection="Horizontal">
+        </asp:RadioButtonList>
+    </div>
+    </form>
+</body>
+</html>
---
+++ previousPages/WebSite1/MyDateTime.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class MyDateTime : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        DateTime d = DateTime.Today;
+        Response.Write((int)d.DayOfWeek);
+    }
+
+    protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+    {
+        //msg.Text = Calendar1.SelectedDate.ToString("d");
+        DateTime d = Calendar1.SelectedDate;
+        for (int i = 0; i < 7; i++)
+        {                     
+            if ((int)d.AddDays(-i).DayOfWeek == 3)
+            {
+                msg.Text += string.Format("<a href='{0}'>CNN {1}</a><br />", getUrl(d.AddDays(-i)), d.AddDays(-i).ToString("d"));
+                d = d.AddDays(-i);
+                RadioButtonList1.Items.Add(d.ToString("d"));
+                break;
+            }
+        }
+
+        for (int i = 0; i < 9; i++)
+        {
+            d = d.AddDays(-7);
+            msg.Text += string.Format("<a href='{0}'>CNN {1}</a><br />", getUrl(d), d.ToString("d"));
+            RadioButtonList1.Items.Add(d.ToString("d"));
+        }
+
+    }
+
+    private string getUrl(DateTime d)
+    {
+        string res = "http://www.liveabc.com/epaper/cnn/{0}/{1:00}/{2:00}/paper.asp";
+        int year = d.Year;
+        int month = d.Month;
+        int day = d.Day;
+        res = string.Format(res, year, month, day);
+        return res;
+    }
+
+    protected void RadioButtonList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        DateTime d = DateTime.Parse(RadioButtonList1.SelectedValue);
+        Response.Redirect(getUrl(d));
+    }
+}
