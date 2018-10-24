a864d0a064571e43bcf7807d5d4796cf5fcb4ecf
wfhsiao <wfhsiao@users.noreply.github.com>
2018-10-24 11:38:18 +0800

    Add files via upload

---
+++ MyImageButton/MyImageButton.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="MyImageButton.aspx.cs" Inherits="MyImageButton" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <style>
+html, body, div, span, object, form, input, h1, h2, button, label, a, img
+{
+    -moz-user-select: none;
+    -webkit-user-select: none; /* this will work for QtWebKit in future */
+    -webkit-user-drag: none;
+}
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:ImageButton ID="ImageButton1" runat="server" OnClick="ImageButton1_Click" />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ MyImageButton/MyImageButton.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class MyImageButton : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Random r = new Random();
+        int v = r.Next(0, 52);
+        string[] s = { "s", "h", "d", "c" };
+        string[] p = { "a", "2", "3", "4", "5", "6", "7", "8", "9", "t", "j", "q", "k" };
+
+        ImageButton1.ImageUrl = String.Format("~/poker/{0}{1}.bmp", p[v % 13], s[v / 13]);
+        Response.Write(v);
+    }
+
+    protected void ImageButton1_Click(object sender, ImageClickEventArgs e)
+    {
+        Response.Output.Write("({0},{1})<br>", e.X, e.Y);
+    }
+}
