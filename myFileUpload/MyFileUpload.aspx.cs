24abbf1089e45180ecd4394bbf151b59b57d953a
wfhsiao <wfhsiao@users.noreply.github.com>
2018-10-31 09:28:52 +0800

    Add files via upload

---
+++ myFileUpload/MyFileUpload.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class MyFileUpload : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        if (FileUpload1.HasFile)
+        {
+            string fn = FileUpload1.FileName;
+            FileUpload1.SaveAs(Server.MapPath("upload/"+fn));
+            HyperLink1.NavigateUrl = "upload/" + fn;
+            HyperLink1.Text = fn;
+        }
+    }
+}
