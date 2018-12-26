18b21d110f4fb8b44db087ea1d17bbe457363132
wfhsiao <wfhsiao@users.noreply.github.com>
2018-12-26 11:35:35 +0800

    Add files via upload

---
+++ TextFromUrl/GeneratingDS.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class GeneratingDS : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        SqlConnection myConn = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Database.mdf;Integrated Security=True");
+        string sqlStr = "select * from words";
+        SqlDataAdapter myCommand = new SqlDataAdapter(sqlStr, myConn); // 'sqlStr is a plain select columns from table where ...
+        DataSet ds = new DataSet();
+        myCommand.Fill(ds);
+        GridView1.DataSource = ds;
+        GridView1.DataBind();
+    }
+}
