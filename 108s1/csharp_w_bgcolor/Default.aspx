6efc5d1844894c575264969919f852b386b037a3
wfhsiao <wfhsiao@users.noreply.github.com>
2019-09-18 17:16:44 +0800

    Add files via upload

---
+++ 108s1/csharp_w_bgcolor/Default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body id="mybody" runat="server">
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox><asp:Button ID="Button1" runat="server" Text="OK" OnClick="Button1_Click" />
+        <br />
+        <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+        </asp:DropDownList>
+        </div>
+    </form>
+</body>
+</html>
---
+++ 108s1/csharp_w_bgcolor/Default.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //int A = 3;
+        //int B = 12;
+        //Response.Output.Write("Hello {1}: {0}<br />", A, B); // slot filling
+        //Circle c1 = new Circle();
+        //Response.Write(c1);
+        if (! IsPostBack)
+        {
+            string[] colors = { "red", "orange", "yellow", "green", "blue", "indigo", "purple" };
+            DropDownList1.DataSource = colors;
+            DropDownList1.DataBind();
+        }
+        
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        int score = Convert.ToInt16(TextBox1.Text);
+        switch (score / 10)
+        {
+            case 10:
+            case 9:Response.Write("優等");
+                break;
+            case 8:Response.Write("甲等");
+                break;
+            case 7:Response.Write("乙等");
+                break;
+            case 6:Response.Write("丙等");
+                break;
+            default:Response.Write("不及格");
+                break;
+        }         
+                    
+
+    }
+
+    protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        mybody.Attributes["bgcolor"] = DropDownList1.SelectedValue;
+    }
+}
+
+class Circle
+{
+    override public string ToString()
+    {
+        return "I'm a circle.";
+    }
+}
