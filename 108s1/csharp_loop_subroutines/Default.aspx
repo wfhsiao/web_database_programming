5dbc846c69fe39cbaa4b220a841f559108326e60
wfhsiao <wfhsiao@users.noreply.github.com>
2019-09-25 16:17:47 +0800

    Add files via upload

---
+++ 108s1/csharp_loop_subroutines/Default.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body id="mybody" runat="server">
+    <form id="form1" runat="server">
+        <div>
+            <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+            </asp:DropDownList>
+            <asp:Button ID="Button1" runat="server" Text="OK" OnClick="Button1_Click" />
+            <br />
+            <br />
+            從<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            一直加到<asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            等於<asp:Literal ID="Literal1" runat="server"></asp:Literal>
+            <br />
+            <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="計算" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ 108s1/csharp_loop_subroutines/Default.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (! IsPostBack)
+        {
+            myInit();
+        }
+        //for (int k = 0; k < 10; k++)
+        //{
+        //    Response.Output.Write("{0}", k);
+        //    if (k != 9)
+        //        Response.Write(", ");
+        //}
+    }
+
+    private void myInit()
+    {
+        string[,] colors = { { "紅", "red" }, { "橙", "orange" }, { "黃", "yellow" }, { "綠", "green" } };
+        // string[] c_colors = { "紅", "橙", "黃", "綠" };
+        for (int i = 0; i < colors.GetLength(0); i++)
+        {
+            DropDownList1.Items.Add(new ListItem(colors[i,0], colors[i,1]));
+        }
+
+        //foreach (string x in colors)
+        //{
+        //    DropDownList1.Items.Add(x);
+        //}
+    }
+
+    protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        mybody.Attributes["bgcolor"] = DropDownList1.SelectedValue;
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        int start = Convert.ToInt16(TextBox1.Text);
+        int end = Convert.ToInt32(TextBox2.Text);
+        long sum = mySum(start, end);
+        Literal1.Text = Convert.ToString(sum);
+    }
+
+    private long mySum(int start, int end)
+    {
+        long sum = 0;
+        for (int i = start; i <= end; i++)
+        {
+            sum += i;
+        }
+        return sum;
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        DropDownList1_SelectedIndexChanged(sender, e);
+    }
+}
