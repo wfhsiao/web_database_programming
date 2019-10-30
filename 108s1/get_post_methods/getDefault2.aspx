1313326e322f3a55541974a3938d690146a9333e
wfhsiao <wfhsiao@users.noreply.github.com>
2019-10-30 11:25:58 +0800

    Add files via upload

---
+++ 108s1/get_post_methods/getDefault2.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="getDefault2.aspx.cs" Inherits="_Defaultget" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Literal ID="msg" runat="server"></asp:Literal>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="清空" />
+        <a href="getTest2.html">getTest2.html</a></div>
+    </form>
+</body>
+</html>
---
+++ 108s1/get_post_methods/getDefault2.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Defaultget : System.Web.UI.Page
+{
+    static string current = "";
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string i = Request["i"];
+        string j = Request["j"];
+        if (i == null || j == null || i.Equals("") || j.Equals(""))
+            Response.Redirect("getTest2.html");
+        current += string.Format("<img src='images/{0}.gif' /><img src='images/{1}.gif' />", i, j);
+        msg.Text = current;
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        current = "";
+        msg.Text = "";
+    }
+}
