a238cf70b03faec48cdf1c4475a03acaae6a0a1c
wfhsiao <wfhsiao@users.noreply.github.com>
2019-11-27 10:06:45 +0800

    Add files via upload

---
+++ 108s1/showDigitsImg/Default.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+        </asp:DropDownList>
+        <asp:DropDownList ID="DropDownList2" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownList2_SelectedIndexChanged">
+        </asp:DropDownList>
+        <br />
+        <asp:Literal ID="Literal1" runat="server"></asp:Literal>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ 108s1/showDigitsImg/Default.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (! IsPostBack)
+        {
+            int[] cs = { 1, 11, 111, 1111 };
+            int[] ds = { 9, 19, 29 };
+            DropDownList1.DataSource = cs;
+            DropDownList2.DataSource = ds;
+            DropDownList1.DataBind();
+            DropDownList2.DataBind();
+            doTimes();
+
+            string c = Request["c"];
+            string d = Request["d"];
+            if (c != null && d != null)
+            {
+                DropDownList1.Visible = false;
+                DropDownList2.Visible = false;
+                int cv = Convert.ToInt32(c);
+                int dv = Convert.ToInt32(d);
+                long res = cv * dv;
+                Literal1.Text = string.Format("{0} x {1} = {2}", cv, dv, res);
+                showImages(Literal1.Text);
+            }
+        }
+    }
+
+    private void showImages(string text)
+    {
+        string res = "<br />";
+        char[] cs = text.ToCharArray();
+        for (int i=0; i<cs.Length; i++)
+        {
+            if (cs[i]>='0' && cs[i]<='9')
+            {
+                res += string.Format("<img src='images/{0}.gif' />", cs[i]);
+            } else
+            {
+                res += cs[i];
+            }
+        }
+        Literal1.Text += res;
+    }
+
+    private void doTimes()
+    {
+        int cv = Convert.ToInt32(DropDownList1.SelectedValue);
+        int dv = Convert.ToInt32(DropDownList2.SelectedValue);
+        long res = cv * dv;
+        Literal1.Text = string.Format("{0} x {1} = {2}", cv, dv, res);
+        showImages(Literal1.Text);
+    }
+
+    protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        doTimes();
+    }
+
+    protected void DropDownList2_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        doTimes();
+    }
+}
